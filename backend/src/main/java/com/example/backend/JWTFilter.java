fdc97ac5a8298e5942e20e911b285a9fb52b3290
SlawomirWenc <slawomirwenc@gmail.com>
2021-06-14 20:30:36 +0200

    Created adding books to 'my library', update jwt token

---
+++ backend/src/main/java/com/example/backend/JWTFilter.java
@@ -0,0 +1,34 @@
+package com.example.backend;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Set;
+
+public class JWTFilter extends BasicAuthenticationFilter {
+
+    private String secret;
+
+
+    public JWTFilter(AuthenticationManager authenticationManager) {
+        super(authenticationManager);
+        this.secret = secret;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        System.out.println(request.getHeader("Authorization"));
+        System.out.println(response.getHeader("Authorization"));
+    }
+}
